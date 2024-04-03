a31383bc685ad8f4e6761f8e9f0815cb968476af
Denis Begovic <denni.begovic@gmail.com>
2024-04-04 01:16:08 +0200

    Deleted everything, starting over

--- src/calculator.js
+++ src/calculator.js
@@ -1,34 +1,24 @@
 class Calculator {
     constructor() {
-        this._expression = '((48/2+6)+20)*2';
-        this._answer = 0;
-        this._operations = ['*', '/', '+', '-', '(', ')'];
-        this._operationPriority = [['*', '/'], ['+', '-']];
-    }
-
-    get expression() {
-        let expression = `${this._expression}&`
-        return expression;
-    }
-
-    get answer() {
-        return this._answer;
-    }
-
-    set answer(output) {
-        this._answer = output; 
-    }
-
-    get operations() {
-        return this._operations;
-    }
-
-    get operationPriority() {
-        return this._operationPriority;
+        this.expression = "(345.2÷22)+35.5";
+        this.output = 0;
+        // Changing operator sign or adding new operations go through here (this.operator)
+        this.operator = {
+            plus: '+',
+            minus: '-',
+            divide: '÷',
+            multiply: 'x',
+            openParenthasis: '(',
+            closeParenthasis: ')'
+        }
+        this.operationPriority = [[this.operator.multiply, this.operator.divide], [this.operator.plus, this.operator.minus]];
     }
 
 
-    //Simple operation functions
+                                                                                                    //         MAIN PART OF THE PROGRAM           //
+
+
+    // All primitive functions (can be added on in the future) 
     add(num1, num2) {
         return num1 + num2;
     }
@@ -37,135 +27,84 @@ class Calculator {
         return num1 - num2;
     }
 
-    multiply (num1, num2) {
-        return num1 * num2;
-    }
-
     divide(num1, num2) {
         return num1 / num2;
     }
 
-    //
-    chooseOperation(char) {
-        switch (char) {
-            case '*':
-                return this.multiply;
-                
-            case '/':
-                return this.divide;
-            
-            case '+':
-                return this.add;
-            
-            case '-':
-                return this.subtract;
+    multiply(num1, num2) {
+        return num1 * num2;
+    }
 
-            default:
-                break;
+    // Performs simple operation based on the operator that is passed in as a argument
+    performOperation(num1, num2, operator) {
+        switch (operator) {
+            case this.operator.plus:
+                return this.add(num1, num2);
+            
+            case this.operator.minus:
+                return this.subtract(num1, num2);
+            
+            case this.operator.multiply:
+                return this.multiply(num1, num2);
+
+            case this.operator.divide:
+                return this.divide(num1, num2);
         }
     }
 
-    performOperation(num1, num2, operation) {
-        return operation(num1, num2);
-    }
+    // Returns parsed expression from string in form of a list of numbers and chars (operator sign)
+    parseExpression() {           // Starting as ==>> "(15+7)*2+1"    Output is ==>>  ['(', 15, '+', 7, ')', '*', 2, '+', 1] // 
+        this.expression += '&';   // Symbol for end ==> & //
 
-    parseExpression() {
-        const expression = this.expression;
         let number = '';
         let operator = '';
-        let lastOperatorIndex = 0;
         let parsedExpression = [];
+        let lastIndex = 0;
 
-        for (let i = 0; i < expression.length; i++) {
-            if (this.operations.includes(expression[i]) || expression[i] == '&') {
-                for (let j = lastOperatorIndex; j < i; j++) {
-                    number += expression[j];
+        for (let i = 0; i < this.expression.length; i++) {
+            if (Object.values(this.operator).includes(this.expression[i]) || this.expression[i] == "&") {       // If the char at i position is on of the operators or parenthasis go back and add all numbers to the number variable//
+                for (let j = lastIndex; j < i; j++) {                                                           // Go back before i and add all numbers to the number variable //
+                    number += this.expression[j];
                 }
-                operator = expression[i];
-                number = parseFloat(number);
-                // console.log(`Checking NaN status for ${number}: ${Number.isNaN(number)}`);
-                // console.log(`Values for current itaration: Number=>${number}, Operation=>${operator}\n`);
-
-                if (Number.isNaN(number)) {
-                    parsedExpression.push(operator);
+                operator = this.expression[i];
+                if (number != '') {
+                    parsedExpression.push(parseFloat(number), operator);
                 } else {
-                    parsedExpression.push(number, operator);
+                    parsedExpression.push(operator);
                 }
 
-                operator = '';
                 number = '';
-                lastOperatorIndex = i + 1;
+                operator = '';
+                lastIndex = i + 1;
             }
         }
-        parsedExpression.pop();
-        return parsedExpression;
-    }
-
-
-    returnExpressionFromParenthasis(expression) {
-        let openIndex;
-        let closeIndex;
-        for(let i = 0; i <= expression.length; i++) {
-            if (expression[i] == '('){
-                openIndex = i;
-            } else if (expression[i] == ')') {
-                closeIndex = i;
-                break;
-            }
-        }
+        parsedExpression.pop() // Removes the end symbol //
         
-        return {
-            openIndex,
-            closeIndex,
-            inParenthasis: expression.slice(openIndex + 1, closeIndex)
-        }
+        console.log(`\Begining expression is: || ${this.expression} ||`);
+        console.log(`Pretty expression is: || ${parsedExpression.join('')} ||`);
     }
 
-
-    calculate(currentExpression) {
-        console.log(`Currently checking sub-expression: ${currentExpression.join('')}`)
-        let num1, num2, result, operation;
-        this.operationPriority.forEach(order => {
-            for (let i = 0; i < currentExpression.length; i++) {
-                if (order.includes(currentExpression[i])) {
-                    num1 = currentExpression[i - 1];
-                    num2 = currentExpression[i + 1];
-                    operation = this.chooseOperation(currentExpression[i]);
-                    result = this.performOperation(num1, num2, operation);
-
-                    currentExpression.splice(i - 1, 3, result);
-                    this.calculate(currentExpression);
-                }
-            }
-        });
-        return currentExpression[0];
+    // Checks for expressions in parenthasis and returns one sub-expression to be calculated
+    handleParenthasis() {
+        
     }
+    
+    // Calculates the expression passed in
+    calculate(expression) {
+        
+    }   
 
-    calculateSubExpressions(expression) {
-        console.log(`Calculating this expression: ${expression.join('')}`);
-        let subExpression= this.returnExpressionFromParenthasis(expression);
-        let currentExpression = subExpression.inParenthasis;
-        let result = this.calculate(currentExpression);
-
-        expression.splice(subExpression.openIndex, subExpression.closeIndex - subExpression.openIndex + 1, result);
-
-        if (expression.includes('(')) {
-            this.calculateSubExpressions(expression);
-        }   
-        return expression;
-    }
-
+    // Helper function that acts as an entry way for all other and sets the this.output to the result of the calculate function
     getResult() {
         let expression = this.parseExpression();
-
-        if (expression.includes('(')) {
-            expression = this.calculateSubExpressions(expression);
-        } 
-        this.answer = this.calculate(expression);
-        console.log(`Final output is: ${this.answer}`);
     }
+
+
+
+
+                                                                                                         //         HELPER FUNCTIONS         //
 
 }
 
-
-export default Calculator;
+const calc = new Calculator();
+calc.parseExpression();
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import Calculator from "./calculator";
+import Calculator from "./calculator.js";
 
 const calculator = new Calculator();
 
 
 $(".btn").on("click", (event) => {
-    const data = mouse.extractRelevantData(event.target);
-    calculator.handle(data);
+    
 });
 
-$(window).on("keyup", (event) => {
-    if (keyboard.acceptableKeys.includes(event.key)) {
-        const data = keyboard.extractRelevantData(event.key);
-        calculator.handle(data);
-    }
-})
+
+// $(window).on("keyup", (event) => {
+//     console.log(event.key);
+// })
