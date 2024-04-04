a8523658dfc140843bc0395fb4757dd9bf9c0059
Denis Begovic <denni.begovic@gmail.com>
2024-04-04 03:15:45 +0200

    Finished the main functions of calculator class, being able to calculate an expression with classic operators and parenthasis

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,6 @@
 class Calculator {
     constructor() {
-        this.expression = "(345.2÷22)+35.5";
+        this.expression = "36÷3+(2x2+(16x0.25))";
         this.output = 0;
         // Changing operator sign or adding new operations go through here (this.operator)
         this.operator = {
@@ -80,31 +80,82 @@ class Calculator {
         }
         parsedExpression.pop() // Removes the end symbol //
         
-        console.log(`\Begining expression is: || ${this.expression} ||`);
         console.log(`Pretty expression is: || ${parsedExpression.join('')} ||`);
+        return parsedExpression;
     }
 
-    // Checks for expressions in parenthasis and returns one sub-expression to be calculated
-    handleParenthasis() {
-        
-    }
-    
-    // Calculates the expression passed in
+    // Calculates the expression passed in //
     calculate(expression) {
-        
+        console.log(`\nIn calculate function. Expression is || ${expression.join('')} ||`);
+        if (expression.includes('(')) {    // Reduces the expression so it doesn't have anymore parenthasis
+            let simplifiedExpression = this.handleParenthasis(expression);
+            this.calculate(simplifiedExpression);
+        } 
+        this.operationPriority.forEach(order => {
+            console.log(`Checking for || ${order} ||`);
+            let num1, num2, result, operator;
+            for (let i = 0; i < expression.length; i++) {
+                if(order.includes(expression[i])) {
+                    operator = expression[i];
+                    num1 = expression[i - 1];
+                    num2 = expression[i + 1];
+
+                    result = this.performOperation(num1, num2, operator);
+                    expression.splice(i - 1, 3, result);   // Replaces the result with the numbers and operator in the array expression //
+                    
+                    i = 0;  // Starts loop from begining //
+
+                    console.log(`|| ${num1} ${operator} ${num2} = ${result} ||`);
+                    console.log(`Expression after this iteration: || ${expression}\n`);
+                }
+            }
+        });
+        console.log(`Result of calculate function is || ${expression}`);  // Returns the only element in the array //
+        return expression[0];
     }   
 
+
+
+    // Extracts the sub-expression inside the parenthasis and passes it to the calculate function
+    // The calculate function returns a number
+    // Then replaces the returned value with the expression in the parenthasis
+    // Once that is done calls the calculate with the simplified expression
+    handleParenthasis(expression) {
+        console.log(`\nInside the handleParenthasis function with the value of: ${expression.join('')}`);
+        let open, close;  // open and close => variables for storing the opening and closing indexes of parenthasis
+        let subExpression = [];
+        for (let i = 0; i < expression.length; i++) {   // Get the last position of the open parenthasis
+            if (expression[i] == '(') {
+                open = i;
+            } 
+        }
+        for (let j = open; j < expression.length; j++) {  // Get the first position of the close parenthasis after the opening
+            if (expression[j] == ')') {
+                close = j;
+                break;
+            }
+        }
+        for (let k = open + 1; k < close; k++) {         // Extract the subexpression    
+            subExpression.push(expression[k]);
+        }
+        console.log(`Extracted sub-expression is || ${subExpression} ||`);
+        let result = this.calculate(subExpression);     // Passes that subexpression in tu the calculate function and returns a result
+        expression.splice(open, close - open + 1, result);  // Replaces the result with the parenthasis group
+        console.log(`Result of sub-expression is || ${result}`);
+        console.log(`Simplified expression is || ${expression.join('')}`);
+        return expression;
+    }
+    
     // Helper function that acts as an entry way for all other and sets the this.output to the result of the calculate function
     getResult() {
         let expression = this.parseExpression();
+        this.output = this.calculate(expression);
+        console.log(this.output);
     }
-
-
-
 
                                                                                                          //         HELPER FUNCTIONS         //
 
 }
 
 const calc = new Calculator();
-calc.parseExpression();
+calc.getResult();
